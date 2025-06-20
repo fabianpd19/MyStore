d4089f7dec5dc9c0437c299cca9ef87f123b09b3
fabianpd19 <fapalma1@espe.edu.ec>
2025-06-20 07:35:44 -0500

    test2

--- pass-hash.js
+++ pass-hash.js
@@ -2,7 +2,7 @@ const bcryp = require('bcrypt');
 
 const myPassword = 'admin123';
 
-
+// const hash = bcrypt.hash(myPassword, 10);
 
 async function hashPassword() {
   const hash = await bcryp.hash(myPassword, 10);
